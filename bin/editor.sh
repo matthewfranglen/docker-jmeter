c115c8aed7039136094dc460603557fc3bc7a376
Matthew Franglen <matthew@franglen.org>
2015-12-02 22:09:38 +0000

    Provide folder for saving plans in editor

--- bin/editor.sh
+++ bin/editor.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-COMMAND=${0}
+COMMAND=`readlink -f ${0}`
 BIN_FOLDER=`dirname "${COMMAND}"`
 . "${BIN_FOLDER}/lib"
 
 (
-    cd "${ROOT_FOLDER}"/docker/editor
-
-    docker build --tag jmeter/editor .
     docker run                                 \
         --interactive                          \
         --tty                                  \
-        --volume /tmp/.X11-unix:/tmp/.X11-unix \
-        --env DISPLAY=unix$DISPLAY             \
-        --name jmeter-editor                   \
         --rm                                   \
-        jmeter/editor
+        --env DISPLAY=unix$DISPLAY             \
+        --volume /tmp/.X11-unix:/tmp/.X11-unix \
+        --volume "${PLANS_FOLDER}:/plans"      \
+        --name jmeter-editor                   \
+        matthewfranglen/jmeter-editor
 )
